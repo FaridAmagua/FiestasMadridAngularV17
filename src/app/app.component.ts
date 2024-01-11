2bc9ea6c2ee635f2b3f272df0c9736d69b439203
Farid Amagua <farid.amagua@llorenteycuenca.com>
2024-01-11 17:59:06 +0100

    estructura de proyecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./shared/navbar/navbar.component";
 import { FormsComponent } from './compononents/forms/forms.component';
+import { HomeComponent } from "./pages/home/home.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss',
-    imports: [CommonModule, RouterOutlet, NavbarComponent,FormsComponent]
+    imports: [CommonModule, RouterOutlet, NavbarComponent, FormsComponent, HomeComponent]
 })
 export class AppComponent {
   title = 'pulseMadrid';
--- src/app/compononents/forms/forms.component.ts
+++ src/app/compononents/forms/forms.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from '../../shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-forms',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule,NavbarComponent],
   templateUrl: './forms.component.html',
   styleUrl: './forms.component.scss'
 })
