3b92268604b939e3c4a5dd9653bb3b36bcb6ba87
Farid Amagua <farid.amagua@llorenteycuenca.com>
2024-01-25 17:55:54 +0100

    segments

--- src/app/core/module1/pages/form/form.component.ts
+++ src/app/core/module1/pages/form/form.component.ts
@@ -1,18 +1,37 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, RouterOutlet, UrlSegment } from '@angular/router';
 
 @Component({
-    selector: 'app-form',
-    standalone: true,
-    templateUrl: './form.component.html',
-    styleUrl: './form.component.scss',
-    imports: [RouterOutlet,CommonModule]
+  selector: 'app-form',
+  standalone: true,
+  templateUrl: './form.component.html',
+  styleUrl: './form.component.scss',
+  imports: [RouterOutlet, CommonModule],
 })
-export class FormComponent {
-    visible: boolean = true;
+export class FormComponent implements OnInit {
+  ngOnInit(): void {
+    // Accede a los segmentos de la URL utilizando el snapshot del ActivatedRoute
+    const segments: UrlSegment[] = this.route.snapshot.url;
 
-    toggleComponent() {
-      this.visible = !this.visible;
-    }
+    // Convierte los segmentos en un array de strings usando map
+    const segmentStrings: string[] = segments.map((segment) => segment.path);
+    
+    console.log(segmentStrings)
+
+    // Verifica si la ruta actual es un route hijo específico
+    this.showContent = !segmentStrings.includes('form-contact');
+    console.log(this.showContent)
+
+    // throw new Error('Method not implemented.');
+  }
+
+  visible: boolean = true;
+  showContent: boolean = true;
+  // get route
+  constructor(private route: ActivatedRoute) {}
+
+  toggleComponent() {
+    this.visible = !this.visible;
+  }
 }
